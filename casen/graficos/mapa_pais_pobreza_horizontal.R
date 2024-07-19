a99e43c29f2b2b63cfb45c2130eca268a859cb15
bastianolea <bastianolea@gmail.com>
2024-07-19 00:09:22 -0400

    mapa de chile horizontal

--- casen/graficos/mapa_pais_pobreza_horizontal.R
+++ casen/graficos/mapa_pais_pobreza_horizontal.R
@@ -32,14 +32,16 @@ mapa_region_datos |>
   guides(fill = guide_none()) +
   coord_sf(xlim = c(-76, -67))
 
+# rotar mapa de Chile ----
 
 # https://gist.github.com/ryanpeek/99c6935ae51429761f5f73cf3b027da2
 # rotate function (see here: https://r-spatial.github.io/sf/articles/sf3.html#affine-transformations
-rotate <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
+# rotate <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
+# rotate(-pi/2)
 
-# make a new geometry that is rotated 90 degrees (but retain original geom)
-mapa_region_datos_rotado <- mapa_region_datos %>% 
-  mutate(geometry = geometry * rotate(-pi/2)) 
+mapa_region_datos_rotado <- mapa_region_datos |> 
+  # mutate(geometry = geometry * rotate(-pi/2)) 
+  mutate(geometry = geometry * matrix(c(0, -1, 0.85, 0), 2))
 
 mapa_region_datos_rotado |> 
   ggplot(aes(fill = pobreza_p, geometry = geometry)) +
@@ -47,4 +49,11 @@ mapa_region_datos_rotado |>
   scale_fill_gradient(low = "grey80", high = "#581695", limits = c(0, NA)) +
   theme_void() +
   guides(fill = guide_none()) +
-  coord_sf(ylim = c(-76, -67))
+  coord_sf(ylim = c(-64.5, -57), 
+           xlim = c(19, 54.5)) +
+  labs(title = "Mapa de Chile horizontal",
+       subtitle = "A mimir") +
+  theme(plot.margin = unit(rep(.2, 4), "cm"))
+
+ggsave("casen/graficos/mapa_chile_horizontal.jpg", 
+       width = 8, height = 3)
